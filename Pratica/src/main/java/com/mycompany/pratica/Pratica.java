8e09a7d1f6e41b28801c75db6b4b074b3a91785d
PedrohAmancio <pedro.h.rosa10@aluno.senai.br>
2024-08-09 21:23:55 -0300

    pratica

---
+++ Pratica/src/main/java/com/mycompany/pratica/Pratica.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pratica;
+
+/**
+ *
+ * @author p.rosa
+ */
+public class Pratica {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 1; i++){
+            System.out.println(i);
+        }
+        for(int i = 10; i > 0; i--){
+             System.out.println(i);
+        }
+    }
+}
