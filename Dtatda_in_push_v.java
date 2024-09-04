4fa8fa9224e10beaf3ca1d0c70cea02df8f26c83
PedrohAmancio <pedro.h.rosa10@aluno.senai.br>
2024-09-04 19:25:12 -0300

    Add files via upload

---
+++ Dtatda_in_push_v.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dtatda_in_push_v;
+
+/**
+ *
+ * @author p.rosa
+ */
+public class Dtatda_in_push_v {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
