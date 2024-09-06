c1128a59f1281fdfcd59b5b7ced72165b5337927
PedrohAmancio <pedro.h.rosa10@aluno.senai.br>
2024-09-06 15:19:47 -0300

    Add files via upload

--- PROJETO_13_PedroH.java
+++ PROJETO_13_PedroH.java
@@ -13,59 +13,122 @@ import java.util.Scanner;
 public class PROJETO_13_PedroH {
 public static int n=0;
     public static void main(String[] args) {
-        int opr;
+        int op = 0;  String gene_tri; int tamanho = 0;
+        int ind1 = 0; int ind2 = 0;
+        
         Scanner ler = new Scanner(System.in);
         
-        System.out.println("Quantos nueros serao calculados");
-        n=ler.nextInt();
-
-        System.out.println("Qual operacao deseja (1-adicao, 2-subtracao" + 
-        "3-multiplicacao ou 4-divisao)");
-        opr=ler.nextInt();
-        if(opr==1){
-            adição(opr);
-        }else if(opr==2){
-            
-        }else if(opr==3){
-            
-        }else if(opr==4){
-            
-    }
-
-    }
-public static void adição(int opr) {
-         Scanner ler = new Scanner(System.in);
-         
-        int []op = new int [n] ; int res;int ind;
-        
-       for(int i = 0; i < n ; ++i){
-        System.out.println("Digite o  numero a ser calculado");
-        op[i]=ler.nextInt();
+       System.out.println("Qual o tamanho do vetor");
+       gene_tri=ler.next();
+       
+       try {// verificação
+          tamanho = Integer.parseInt(gene_tri);
+          if (tamanho <= 0) {
+            System.out.println("Erro: tamanho do vetor invalido.");
+            }
+       } catch (NumberFormatException e) {
+            System.out.println("Erro: argumento invalido");
        }
        
-        for(int i = 0; i < n ; ++i){
-        System.out.println("Esses são os numeros dentro do vetor" + i);
-                System.out.println(op[i]);
-           
+        while (tamanho <= 0);
+
+        int vet[] = new int[tamanho];
+        double resultados[] = new double[tamanho];
+
+        for (int i = 0; i < tamanho; i++) {/* vai digitar o numero ate o desejado,
+            ler, fazer verificação e deixar em um "mini loop"*/
+            do {
+                System.out.print("Digite um numero: ");
+                gene_tri = ler.next();
+                try {
+                    vet[i] = Integer.parseInt(gene_tri);
+                } catch (NumberFormatException e) {
+                    System.out.println("Erro: argumento invalido.");
+                }
+            } while (vet[i] == 0 && !gene_tri.equals("0"));
+            if (vet[i] < 0) {
+                System.out.println("Erro: argumento invalido.");
+                i--;
+            }
         }
-        System.out.println("Quais serao somados (Digite o indice");
-        ind = ler.nextInt();
-        
-        try{
-            System.out.println("Valor no indice"+ ind+":"+ op[ind]);
-    }catch (ArrayIndexOutOfBoundsException e){
-        System.out.println("Erro: indice fora do intervalo");
-    }
+
+        for (int i = 0; i < tamanho; i++) {//vai jogar o numero e o indice na tela
+            System.out.println("Numero: " + vet[i] + " Indice: " + i);
         }
-        
-       
-    public static void subtração(String[] args) {
-        System.out.println("Hello World!");
-    }
-    public static void divisão(String[] args) {
-        System.out.println("Hello World!");
-    }
-    public static void multiplicação(String[] args) {
-        System.out.println("Hello World!");
+
+        for (int i = 0; i < tamanho; i++) {/* pergunta a operação, faz verificação
+            começa o processo de calculo(pergunta os numeros), faz verificação
+            entra no escolha caso para mostrar resultado e "quebra o codigo"(na
+            divisão faz verificação)*/
+
+            do {
+                System.out.println("Escolha: \n 1 - Soma \n 2 - Subtracao \n 3 - Multiplicacao \n 4 - Divisao");
+                gene_tri = ler.next();
+                try {
+                    op = Integer.parseInt(gene_tri);
+                    if (op > 4 || op < 0) {
+                        System.out.println("Erro: operacao invelida.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Erro: argumento invalido.");
+                }
+            } while (op > 4 || op <= 0);
+
+            do {
+                System.out.println("Escolha o primeiro indice: ");
+                gene_tri = ler.next();
+                try {
+                    ind1 = Integer.parseInt(gene_tri);
+                    if (ind1 >= tamanho || ind1 < 0) {
+                        System.out.println("Erro: primeiro indice invalido.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Erro: primeiro indice invalido.");
+                }
+            } while (ind1 < 0 || ind1 >= tamanho || ind1 == 0 && !gene_tri.equals("0"));
+
+            do {
+                System.out.println("Escolha o segundo indice: ");
+                gene_tri = ler.next();
+                try {
+                    ind2 = Integer.parseInt(gene_tri);
+                    if (ind2 >= tamanho || ind2 < 0) {
+                        System.out.println("Erro: segundo indice invalido.");
+                    }
+                } catch (NumberFormatException e2) {
+                    System.out.println("Erro: segundo indice invalido.");
+                }
+            } while (ind2 < 0 || ind2 >= tamanho || ind2 == 0 && !gene_tri.equals("0"));
+
+            switch (op) {
+                case 1:
+                    resultados[i] = vet[ind1] + vet[ind2];
+                    System.out.println("Resultado " + (i + 1) + ": " + resultados[i]);
+                    break;
+                case 2:
+                    resultados[i] = vet[ind1] - vet[ind2];
+                    System.out.println("Resultado " + (i + 1) + ": " + resultados[i]);
+                    break;
+                case 3:
+                    resultados[i] = vet[ind1] * vet[ind2];
+                    System.out.println("Resultado " + (i + 1) + ": " + resultados[i]);
+                    break;
+                case 4:
+                    try {
+                        double num1 = vet[ind1], num2 = vet[ind2];
+                        resultados[i] = num1 / num2;
+                        System.out.println("Resultado " + (i + 1) + ": " + resultados[i]);
+                    } catch (ArithmeticException e) {
+                        System.out.println("Erro: calculo indisponivael");
+                        i--;
+                    }
+                    break;
+            }
+
+        }
+        System.out.println("\nResultados dos calculos:");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.println("Resultado "+(i+1)+":"+resultados[i]);
+        }
     }
 }
