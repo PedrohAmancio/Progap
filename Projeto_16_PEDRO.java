39c9a61507ac58c486d6d9bd329da45fb3e99f9f
PedrohAmancio <pedro.h.rosa10@aluno.senai.br>
2024-09-30 21:37:48 -0300

    Add files via upload

---
+++ Projeto_16_PEDRO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_16_pedro;
+
+/**
+ *
+ * @author p.rosa
+ */
+public class Projeto_16_PEDRO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
