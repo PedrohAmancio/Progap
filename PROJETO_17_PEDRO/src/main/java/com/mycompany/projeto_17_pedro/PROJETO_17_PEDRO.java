ea6df7e77bd3ca1a1602f783a4a01014fa788d47
Pedroh Amancio <pedro.h.rosa10@aluno.senai.br>
2024-10-17 20:45:32 -0300

    Pedro_Projeto_17

---
+++ PROJETO_17_PEDRO/src/main/java/com/mycompany/projeto_17_pedro/PROJETO_17_PEDRO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_17_pedro;
+
+/**
+ *
+ * @author p.rosa
+ */
+public class PROJETO_17_PEDRO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
